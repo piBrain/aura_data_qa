4bb8682f94df70e94e91d16cead66007fb120b63
Watson Markson <watson@pibrain.io>
2017-04-21 14:39:35 -0400

    add 'sites' table to track domain rankings and priority

---
+++ api/src/db/sequelize/site.js
@@ -0,0 +1,23 @@
+'use strict';
+module.exports = function(sequelize, DataTypes) {
+  var Site = sequelize.define('Site', {
+    id: { 
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true,
+      type: DataTypes.INTEGER,
+    },
+    quantcast_rank: DataTypes.INTEGER,
+    url: DataTypes.STRING,
+    priority_domain: DataTypes.BOOLEAN,
+  }, {
+    classMethods: {
+      associate: function(models) {
+        // associations can be defined here
+      }
+    }
+  });
+  return Site;
+};
+
+
