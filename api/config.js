24e0cd9ae488f8b8ccfa7e8484b02cd1c275aec2
Ian T Butler <iantbutler@pibrain.io>
2017-05-06 16:38:21 -0400

    System runs locally again.

--- api/config.js
+++ api/config.js
@@ -1,13 +1,18 @@
 const configs = {
   local: {
-    // DATABASE_URL: 'postgres://127.0.0.1:5432/aura_qa_development',
+    DATABASE_URL: 'postgres://127.0.0.1:5432/aura_qa_development',
     GOOGLE_CLIENT_ID: '490447248487-367u8f0drevjtgajlslp4inc8ch2s8f0.apps.googleusercontent.com',
     GSUITE_DOMAIN: 'pibrain.io',
     LISTEN_PORT: '4200',
   },
 }
 
-const { NODE_ENV } = process.env
+let { NODE_ENV } = process.env
+
+if(typeof NODE_ENV === 'undefined') {
+  console.info('NODE_ENV not supplied falling back to local environment.')
+  NODE_ENV = 'local'
+}
 
 module.exports = () => {
   process.env = Object.assign(process.env, configs[NODE_ENV])
